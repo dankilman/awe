7a36c582d67be8606297afd37aae6401943ccace
Dan Kilman <dankilman@gmail.com>
2018-12-22 01:45:36 +0200

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='awe',
-    version='0.4',
+    version='0.5',
     description='Dynamic web based reports/dashboards in python',
     long_description='',
     license='MIT License',
