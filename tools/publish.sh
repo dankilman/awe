27b6aec6e7ea9852fd3fe1034c786341b909b6cd
Dan Kilman <dankilman@gmail.com>
2018-12-27 22:42:56 +0200

    publish script improvements

--- tools/publish.sh
+++ tools/publish.sh
@@ -8,6 +8,7 @@ dist_file="dist/awe-${version}-py2-none-any.whl"
 master_branch="master"
 current_branch="$(git symbolic-ref --short HEAD)"
 origin_master="origin/master"
+build_status_url="https://circleci.com/api/v1.1/project/github/dankilman/awe/tree/${master_branch}?limit=1"
 
 exit_with()
 {
@@ -17,24 +18,30 @@ exit_with()
 
 validate()
 {
+    echo "Running validations"
     test "${current_branch}" = "${master_branch}" || exit_with "Current branch is not ${master_branch}"
     git diff --quiet "${current_branch}" "${origin_master}" || exit_with "local version differs from origin"
     test -f "${dist_file}" || exit_with "no dist found"
+    status="$(curl ${build_status_url} 2> /dev/null | jq '.[].status' -r)"
+    test "${status}" = 'success' || exit_with "Last ${master_branch} build failed"
 }
 
 tag()
 {
+    echo "Tagging and pushing tag"
     git tag "${version}"
     git push origin "${version}"
 }
 
 publish_pypi()
 {
+    echo "Uploading to pypi"
     twine upload "${dist_file}"
 }
 
 publish_static_files()
 {
+    echo "pushing static files to s3://awe-static-files"
     aws --profile awe-publisher s3 cp \
         awe/resources/client/awe/build s3://awe-static-files/dist/${version} \
         --recursive \
@@ -44,6 +51,7 @@ publish_static_files()
 
 main()
 {
+    echo "Publishing version ${version}"
     validate
     tag
     publish_pypi
