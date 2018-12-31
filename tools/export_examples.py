102492f150a07cb112ebad8142ac7fa6dedcd624
Dan Kilman <dankilman@gmail.com>
2019-01-01 01:51:01 +0200

    more cleanup

--- tools/export_examples.py
+++ tools/export_examples.py
@@ -2,11 +2,12 @@ from __future__ import print_function
 
 import errno
 import importlib
-import threading
 import os
+import threading
 import time
 
 from awe import api
+
 
 output_examples_dir = os.path.join(os.path.dirname(__file__), '../published-examples')
 
@@ -31,24 +32,20 @@ examples = {
 }
 
 
-def main():
+def setup():
+    os.environ.update({'AWE_OFFLINE': '1', 'AWE_SET_GLOBAL': '1'})
     try:
         os.makedirs(output_examples_dir)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
 
-    os.environ.update({
-        'AWE_OFFLINE': '1',
-        'AWE_SET_GLOBAL': '1',
-    })
+
+def main():
+    setup()
     for example, config in examples.items():
-        def export_fn(index_html):
-            with open(os.path.join(output_examples_dir, '{}.html'.format(example)), 'w') as f:
-                f.write(index_html)
         print('Processing {}, {}'.format(example, config))
-        module_path = 'examples.{}'.format(example)
-        module = importlib.import_module(module_path)
+        module = importlib.import_module('examples.{}'.format(example))
         thread = threading.Thread(target=module.main)
         thread.start()
         terminate_after = config.get('terminate_after')
@@ -56,7 +53,8 @@ def main():
             time.sleep(terminate_after)
             api.page.close()
         thread.join(timeout=300)
-        api.page.export(export_fn=export_fn)
+        with open(os.path.join(output_examples_dir, '{}.html'.format(example)), 'w') as f:
+            f.write(api.page.export())
 
 
 if __name__ == '__main__':
